22879aa8772cdbb8ef3d77172531a419d6bdb7be
JoelVN <45366944+JoelVN@users.noreply.github.com>
2020-01-29 23:23:32 -0500

    Agregar ingreso de vechiculos en home

--- src/app/pages/messages/messages.page.ts
+++ src/app/pages/messages/messages.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.page.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  vehiculos(){
+    this.router.navigate(['/registrovehiculo']);
   }
 
 }
